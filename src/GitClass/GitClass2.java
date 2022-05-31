7268de67f2d475d9084e9bddcd4d6681959a996c
giselMontano <giselalesita@gmail.com>
2022-05-31 19:54:29 -0400

    2nd commit

--- src/GitClass/GitClass2.java
+++ src/GitClass/GitClass2.java
@@ -4,7 +4,8 @@ public class GitClass2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Today is a good day");
+System.out.println("Today is a good day");
 	}
 
 }
